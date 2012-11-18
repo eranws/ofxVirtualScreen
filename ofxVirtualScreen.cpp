8639677574f663b5d3e4d151624968a498eae2bd
Eran Weissenstern <eran.weissenstern>
2012-11-18 02:04:07 +0200

    scene sensor intersection

---
+++ ofxVirtualScreen.cpp
@@ -0,0 +1,79 @@
+#include "ofxVirtualScreen.h"
+#include "ofAppRunner.h"
+
+
+Scene::Scene(void)
+{
+	screen.setGlobalPosition(0, -50-163, 0);
+	screen.color = ofColor::red; 
+
+	sensor.setGlobalPosition(0, 0, 0);
+	sensor.color = ofColor::yellow; 
+
+}
+
+
+Scene::~Scene(void)
+{
+}
+
+void Scene::customDraw()
+{
+	sensor.draw();
+	screen.draw();
+
+}
+
+
+void SceneItem::customDraw()
+{
+
+	ofFill();
+	ofSetColor(color);
+	ofBox(1);
+
+	ofNoFill();
+	ofSetLineWidth(2);
+	ofSetColor(color.getInverted());
+	ofBox(1);
+
+}
+
+// http://mathworld.wolfram.com/Line-PlaneIntersection.html
+ofPoint Screen::getIntersectionPointWithLine( ofPoint p4, ofPoint p5 )
+{
+	ofPoint hs = getScale() / 2; //= halfScale 
+	ofPoint p1 = getPosition() + ofPoint(+hs.x, +hs.y, 0);
+	ofPoint p2 = getPosition() + ofPoint(-hs.x, +hs.y, 0);
+	ofPoint p3 = getPosition() + ofPoint(-hs.x, -hs.y, 0);
+	
+	float t =
+		-ofMatrix4x4(
+		1   ,1   ,1   ,1   ,
+		p1.x,p2.x,p3.x,p4.x,
+		p1.y,p2.y,p3.y,p4.y,
+		p1.z,p2.z,p3.z,p4.z
+		).determinant()
+		/
+		ofMatrix4x4(
+		1   ,1   ,1   ,0   ,
+		p1.x,p2.x,p3.x,p5.x-p4.x,
+		p1.y,p2.y,p3.y,p5.y-p4.y,
+		p1.z,p2.z,p3.z,p5.z-p4.z
+		).determinant();
+
+	ofPoint res = p4 + (p5-p4) * t;
+
+	return res;
+}
+
+ofVec2f Screen::getScreenPointFromWorld(ofPoint p)
+{
+	ofPoint hs = getScale() / 2; //= halfScale 
+	ofPoint dp = getPosition() + ofPoint(+hs.x, +hs.y, 0);
+	
+	ofVec2f res((dp.x + p.x) * ofGetScreenWidth()  / getScale().x,
+		(dp.y - p.y) * ofGetScreenHeight() / getScale().y);
+
+	return res;
+}
---
+++ ofxVirtualScreen.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "ofNode.h"
+
+class SceneItem : public ofNode
+{
+public:
+	virtual void customDraw();
+	ofColor color;
+};
+
+class Sensor : public SceneItem
+{
+public:
+	Sensor(void)
+	{
+		setScale(200, 15, 30);
+	}
+};
+
+class Screen: public SceneItem
+{
+public:
+	Screen(void)
+	{
+		//setScale(559.7, 366.0, 201.5); //physical
+		setScale(520.0, 326.0, 20); // visual
+	}
+	ofPoint getIntersectionPointWithLine(ofPoint p1, ofPoint p2);
+	ofVec2f getScreenPointFromWorld(ofPoint);
+};
+
+
+
+
+
+class Scene : public ofNode
+{
+public:
+	Scene(void);
+	virtual ~Scene(void);
+
+	virtual void customDraw();
+
+	Screen screen;
+	Sensor sensor;
+
+
+};
+
